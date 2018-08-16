63d8cc19af2929e7182a5be547f19dd1e2f5a736
Bill Leonhardt <iamleonhardt@gmail.com>
2018-08-15 23:47:21 -0700

    refactoring into simple page with pure JS, no framework

--- main.js
+++ main.js
@@ -1,59 +1,78 @@
-var app = angular.module('portfolioApp', ['ngAnimate', 'ui.router']);
+let applicationsArr = [
+  {
+    title: "Truck Route",
+    image: "images/bill.png",
+    text:
+      "Helps trucks find the best departure time by tracking traffic data every 5 mins and telling you exactly when traffic tends to start getting bad.",
+    link: "http://iamleonhardt.com/truckRoute"
+  },
+  {
+    title: "Effect Maker",
+    image: "images/image.jpg",
+    text:
+      "Developers can find effects they want, customize them, and then get the code to create them.",
+    link: "http://iamleonhardt.com/effectMaker"
+  },
+  {
+    title: "Truck Route",
+    image: "images/bill.png",
+    text:
+      "Helps trucks find the best departure time by tracking traffic data every 5 mins and telling you exactly when traffic tends to start getting bad.",
+    link: "http://iamleonhardt.com/truckRoute"
+  },
+  {
+    title: "Effect Maker",
+    image: "images/image.jpg",
+    text:
+      "Developers can find effects they want, customize them, and then get the code to create them.",
+    link: "http://iamleonhardt.com/effectMaker"
+  },
+  {
+    title: "Truck Route",
+    image: "images/bill.png",
+    text:
+      "Helps trucks find the best departure time by tracking traffic data every 5 mins and telling you exactly when traffic tends to start getting bad.",
+    link: "http://iamleonhardt.com/truckRoute"
+  },
+  {
+    title: "Effect Maker",
+    image: "images/image.jpg",
+    text:
+      "Developers can find effects they want, customize them, and then get the code to create them.",
+    link: "http://iamleonhardt.com/effectMaker"
+  }
+];
 
-app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
-    $urlRouterProvider.otherwise('/');
-    $stateProvider
-        .state('main', {
-            url: '/',
-            views: {
-                'body' : {
-                    templateUrl : 'partials/main.html',
-                    controller : 'applicationsController'
+// Main Template
+let applicationTemplate = `
+<div class="nameComponent">
+    <h1 class="name">i<span class="secondaryColor">am</span>leonhardt</h1>
+    <h4>| ux designer | web developer | lover | fighter |</h4>
+</div>
+<div class="aboutComponent">My name is Bill and I've worked in Software Development for the last 2 years.</div>
 
-                }
-            }
-        })
-        .state('applications', {
-            url: '/',
-            views: {
-                'pageName': {
-                    template: '<h4>Applications</h4>'
-                },
-                'body' : {
-                    templateUrl : 'partials/applications.html',
-                    controller : 'applicationsController'
-                }
+`;
 
-            }
-        })
-        .state('about', {
-            url: '/',
-            views: {
-                'pageName': {
-                    template: '<h4>About</h4>'
-                },
-                'body' : {
-                    templateUrl : 'partials/about.html'
-                }
-            }
-        });
-}]);
+// Application Template
+applicationsArr.forEach(app => {
+  applicationTemplate += `
+<div class="card">
+    <div class="cardImage" style="background: linear-gradient(rgba(40,40,40,0.1), rgba(40,40,40,.6)), url(${
+      app.image
+    });">
+        <h2 class="cardTitle">${app.title}</h2>
+    </div>
+    <p class="cardText">${app.text}</p>
+    <div class="cardActionBar">
+        <button class="cardButton" ng-click="detailsModal()">Details</button>
+        <button class="cardButton" ng-click="liveProject(app)">Demo</button>
+    </div>
+</div>
+`;
+});
 
-app.controller('applicationsController', ['$http', '$scope', '$window', function($http, $scope, $window){
+applicationTemplate += `
+<footer>Handcrafted by Bill Leonhardt</footer>
+`;
 
-    $scope.detailsModal = function(){
-        console.log('details clicked, open modal');
-    };
-
-    $scope.liveProject = function(app){
-        console.log('link is : ', app.link);
-            $window.open(app.link, '_blank');
-    }
-
-    $http.get('applicationList.json')
-        .success(function(data){
-        $scope.applications = data.applications;
-        console.log(data);
-    })
-}]);
-
+document.getElementById("content").innerHTML = applicationTemplate;
